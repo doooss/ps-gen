c9800c818f9d7a9d8bda452cecd4ac19de3ba9d8
doyoung <ldy9212@gmail.com>
2023-09-18 15:44:10 +0900

    .

---
+++ cli/templates/hooks/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    description: 'keeping the naming convention (ex use + PascalCase)',
+    prompts: [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the hook?',
+        },
+    ],
+    baseUrl: 'src/hooks',
+    actions: [
+        {
+            type: 'add',
+            path: '{{name}}/{{name}}.test.ts',
+            templateFile: 'test.hbs',
+        },
+        {
+            type: 'add',
+            path: '{{name}}/index.ts',
+            templateFile: 'export.hbs',
+        },
+        {
+            type: 'add',
+            path: '{{name}}/{{name}}.ts',
+            templateFile: 'hook.hbs',
+        },
+        {
+            type: 'append',
+            path: 'index.ts',
+            template: "export { default as {{ name }} } from './{{ name }}'",
+        },
+    ],
+}   
+;
---
+++ index.js
@@ -0,0 +1,97 @@
+const inquirer = require('inquirer') ;
+const handlebars = require('handlebars')
+const fs = require('fs');
+const path = require('path');
+
+const TEMPLATES_ROOT = path.join(__dirname, '/cli/templates');
+
+
+function ensureDirectoryExistence(filePath) {
+    const dirName = path.dirname(filePath);
+    if (fs.existsSync(dirName)) {
+        return;
+    }
+    ensureDirectoryExistence(dirName);
+    fs.mkdirSync(dirName);
+}
+
+async function getGeneratorsFromTemplates() {
+    const directories = fs.readdirSync(TEMPLATES_ROOT, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+
+    const generators = {};
+
+    for (const directory of directories) {
+        const possibleConfigPaths = [
+            path.join(TEMPLATES_ROOT, directory, 'config.js'),
+            path.join(TEMPLATES_ROOT, directory, 'config.ts'),
+            path.join(TEMPLATES_ROOT, directory, 'config.json'),
+        ];
+
+        for (const configPath of possibleConfigPaths) {
+            if (fs.existsSync(configPath)) {
+                const configModule = await require(configPath);
+                if(configModule){
+
+                    generators[directory] = configModule;
+                    break;
+                }
+            }
+        }
+    }
+
+    return generators;
+}
+
+async function promptForGenerator() {
+    const generators = await getGeneratorsFromTemplates();
+    const generatorChoices = Object.keys(generators).map(key => ({
+        name: `${key} - ${generators[key]?.description ?? ''}`,
+        value: key,
+    }));
+
+    const { generatorChoice } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'generatorChoice',
+            message: 'Select a generator:',
+            choices: generatorChoices,
+        },
+    ]);
+
+    return generatorChoice;
+}
+
+async function executeGenerator(generatorKey) {
+    const generators = await getGeneratorsFromTemplates();
+    const generator = generators[generatorKey];
+    const templateBasePath = path.join(TEMPLATES_ROOT,generatorKey );
+    const baseUrl = generator.baseUrl || '';  // baseUrl 값을 가져오거나 없으면 빈 문자열을 사용합니다.
+    const answers = await inquirer.prompt(generator.prompts);
+
+    for (const action of generator.actions) {
+        if(!action.templateFile) continue;
+
+        const templatePath = path.join(templateBasePath, action.templateFile);
+        const templateContent = fs.readFileSync(templatePath, 'utf8');
+        const compiledTemplate = handlebars.compile(templateContent);
+
+        // baseUrl을 포함하여 출력 경로를 조정합니다.
+        const outputPath = path.join(__dirname, '/', baseUrl, action.path.replace(/{{name}}/g, answers.name));
+
+        if (action.type === 'add') {
+            ensureDirectoryExistence(outputPath);
+            fs.writeFileSync(outputPath, compiledTemplate(answers), 'utf8');
+        } else if (action.type === 'append') {
+            const existingContent = fs.existsSync(outputPath) ? fs.readFileSync(outputPath, 'utf8') : '';
+            fs.writeFileSync(outputPath, existingContent + compiledTemplate(answers), 'utf8');
+        }
+    }
+}
+
+(async () => {
+    const generatorChoice = await promptForGenerator();
+    await executeGenerator(generatorChoice);
+    console.log('Files successfully generated!');
+})();
